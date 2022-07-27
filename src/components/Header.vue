5f4e548918ff59eadbc2112d4b052240e62cf8d9
InnocentusLime <InnocentusLime.help@gmail.com>
2022-07-27 22:39:18 +0300

    Replaced compiled code with vue src

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+	<header>
+		<nav>
+			<ul>
+				<li><a class = "button" href="#" role="button">Articles</a></li>
+				<li><a class = "button" href="#" role="button">Projects</a></li>
+				<li><a class = "button" href="#" role="button">Resources</a></li>
+				<li><a class = "button" href="#" role="button">Search</a></li>
+			</ul>
+		</nav>
+	</header>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+</style>
