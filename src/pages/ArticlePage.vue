0bbee4383f0bec72f7f2359b2d8c3ea4d34aefcf
InnocentusLime <InnocentusLime.help@gmail.com>
2022-08-11 23:00:40 +0300

    Added some test article rendering

--- src/pages/ArticlePage.vue
+++ src/pages/ArticlePage.vue
@@ -1,22 +1,34 @@
 <template>
-	Article {{ $route.params.uuid }}!
+	<article class='theArticle'>
+		<p v-if="articleMd === null">Loading article md...</p>
+		<MdBlock v-if="articleMd !== null" :text='articleMd'/>
+	</article>
 </template> 
 
 <script>
+import MdBlock from "./parts/MdBlock"
+//Article {{ $route.params.uuid }}!
 //import MathFormula from "./Subcomponents/MathFormula"
-//import axios from "axios"
+import axios from "axios"
 //import ArticlePanel from "./parts/ArticlePanel"
 
 export default {
-	name: "Middle",
-	components: {},
+	name: "ArticlePage",
+	components: { 
+		MdBlock 
+	},
+  data() {
+    return {
+      articleMd: null,
+    }
+  },
   computed: {},
   beforeMount() {
-		/*
-    axios.get("/articlePanels/page1.json").then(response => {
-      this.articleInfos = response.data;
+    axios.get("/articles/" + this.$route.params.uuid + ".md").then(response => {
+			if (response.status == 200) {
+				this.articleMd = response.data;
+			}
     });
-		*/
   },
 }
 </script>
